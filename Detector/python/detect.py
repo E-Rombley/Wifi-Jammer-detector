9cb70134b257cf025128b26da54f170e511c42b4
E-Rombley <e.rombley2002@gmail.com>
2026-06-02 14:42:50 +0000

    final threshold

--- Detector/python/detect.py
+++ Detector/python/detect.py
@@ -10,7 +10,7 @@ import time
 
 ROUTER_IP       = "192.168.1.1"
 SSH_KEY         = "/home/guy/.ssh/openWrt_key"
-THRESHOLD_DBM   = -50
+THRESHOLD_DBM   = -55
 TRIGGER_SECONDS = 2
 WATERFALL_MIN   = -88
 WATERFALL_MAX   = -20
