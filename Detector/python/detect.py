cdb3c6d5cdfdb594c79c57935638274c465d9884
E-Rombley <e.rombley2002@gmail.com>
2026-06-02 16:07:18 +0000

    fixing

--- Detector/python/detect.py
+++ Detector/python/detect.py
@@ -10,7 +10,7 @@ import time
 
 ROUTER_IP       = "192.168.1.1"
 SSH_KEY         = "/home/guy/.ssh/openWrt_key"
-THRESHOLD_DBM   = -50
+THRESHOLD_DBM   = -37
 TRIGGER_SECONDS = 1
 WATERFALL_MIN   = -88
 WATERFALL_MAX   = -22
